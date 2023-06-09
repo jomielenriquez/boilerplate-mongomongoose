b4cab8e77d63ca1af5d0dfd366acaff47bf1a7a8
Replit user <>
2023-06-09 05:51:04 +0000

    Update

--- myApp.js
+++ myApp.js
@@ -1,7 +1,15 @@
+let mongoose = require("mongoose");
 require('dotenv').config();
 
-
 let Person;
+
+mongoose.connect(process.env['MONGO_URI'],
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  }
+);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
